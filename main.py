18239444258c19163585f62886cf8e116bb33f42
SenaKebe <152234422+SenaKebe@users.noreply.github.com>
2025-06-26 14:56:50 +0300

    TaskBase_model

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from enum import Enum
+from pydantic import BaseModel, Field
+
 
 app = FastAPI(title="Task Manager API")
 
@@ -14,3 +16,8 @@ class PriorityEnum(str, Enum):
     low = "low"
     medium = "medium"
     high = "high"
+class TaskBase(BaseModel):
+    title: str = Field(..., title="Title", example="Write blog post")
+    description: Optional[str] = Field(default=None, title="Description", example="Write about FastAPI")
+    status: StatusEnum = Field(..., title="Status", example="pending")
+    priority: PriorityEnum = Field(..., title="Priority", example="high")
