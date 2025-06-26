5413c5e59ead5a7a607631e22b11b97272cadefe
SenaKebe <152234422+SenaKebe@users.noreply.github.com>
2025-06-26 14:53:39 +0300

    enums for task status and priority

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI(title="Task Manager API")
+
+from enum import Enum
+
+class StatusEnum(str, Enum):
+    pending = "pending"
+    in_progress = "in_progress"
+    completed = "completed"
+
+class PriorityEnum(str, Enum):
+    low = "low"
+    medium = "medium"
+    high = "high"
